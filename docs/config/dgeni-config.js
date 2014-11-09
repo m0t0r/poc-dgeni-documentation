dded8d1798daa94415d93c0de26da9271b1bd159
Vitaly <motor.kodlife@gmail.com>
2014-11-09 13:40:09 -0500

    chore(dgeni): Created basic dgeni config

---
+++ docs/config/dgeni-config.js
@@ -0,0 +1,42 @@
+var path = require('cannonical-path');
+
+var Package = require('dgeni').Package;
+
+module.exports = new Package('dgeni-demo', [
+  require('dgeni-packages/ngdoc'),
+  require('dgeni-packages/examples')
+])
+
+.config(function(dgeni, log, readFilesProcessor, writeFilesProcessor) {
+
+  dgeni.stopOnValidationError = true;
+  dgeni.stopOnProcessingError = true;
+
+  log.level = 'info';
+
+  readFilesProcessor.basePath = path.resolve(__dirname, '..');
+  readFilesProcessor.sourceFiles = [
+    {include: 'src/**/*.js', basePath: 'src'},
+    {include: 'docs/content/**/*.ngdoc', basePath: 'docs/content'}
+  ];
+
+  writeFilesProcessor.outputFolder = 'build/docs';
+})
+
+.config(function(templateFinder, templateEngine) {
+
+  templateEngine.config.tags = {
+    variableStart: '{$',
+    variableEnd: '$}'
+  };
+
+  templateFinder.templateFolders.unshift(path.resolve(__dirname, 'templates'));
+
+  templateFinder.templatePatterns = [
+    '${ doc.template }',
+    '${ doc.id }.${ doc.docType }.template.html',
+    '${ doc.id }.template.html',
+    '${ doc.docType }.template.html',
+    'common.template.html'
+  ];
+});
