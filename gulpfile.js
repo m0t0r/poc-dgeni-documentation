db391e02a3e5bcd6a531616ea3a378219f4e97f3
Vitaly <motor.kodlife@gmail.com>
2014-11-09 12:42:30 -0500

    chore(gulp): Created basic gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
+var Dgeni = require('dgeni');
 
+gulp.task('jshint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('dgeni', ['jshint'], function() {
+  var dgeni = new Dgeni([require('./docs/config/dgeni-config.js')]);
+  return dgeni.generate().catch(function(error) {
+    console.error(error);
+    process.exit(1);
+  });
+});
